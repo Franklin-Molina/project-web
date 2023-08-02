ac887773530debd918169418e76f9ee771ca33ac
Franklin-Molina <asdfglxl7@gmail.com>
2023-08-01 19:14:02 -0500

    completed

---
+++ script.js
@@ -0,0 +1,31 @@
+/* ( () => { */
+
+  import checkComplete from "./components/checkComnplete.js"
+  import deleteIcon from "./components/deleteIcon.js";
+  const btn = document.querySelector("[data-form-btn]");
+  const input = document.querySelector("[data-form-input]");
+
+
+  const createTask = (evento) =>{
+      evento.preventDefault();
+      const input = document.querySelector("[data-form-input]")
+      const value = input.value;
+      const list = document.querySelector("[data-list]")
+      const task = document.createElement('li')
+      task.classList.add('card')
+      input.value = "";
+      //backticks    
+      const taskContent = document.createElement('div');
+      const titleTask = document.createElement('span')
+      titleTask.classList.add('task');
+      titleTask.innerText = value;
+      taskContent.appendChild(checkComplete());
+      taskContent.appendChild(titleTask);    
+      task.appendChild(taskContent);
+      task.appendChild(deleteIcon()) 
+      list.appendChild(task)
+  };
+
+  btn.addEventListener("click", createTask)
+
+//})()  //hacer el codigo seguro con IIFE funciones que se declaran y se ejecutan
